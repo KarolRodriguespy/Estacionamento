a72d36d894438609ce1c0f2f5bba13d3805599ec
amandabrbz <amanda.brbz@hotmail.com>
2019-10-22 22:30:13 -0300

    isto

--- Estacionamento.php
+++ Estacionamento.php
@@ -8,40 +8,31 @@ use \InvalidArgumentException;
 class Estacionamento
 {
     public $carro;
-    private $carrosEstacionados = [];
-    const VAGAS = 12;
+    public $motocicleta;
+    private $veiculosEstacionados = [];
+    const VAGAS = 20;
 
     public function getCarrosEstacionados()
     {
-    return $this -> carrosEstacionados;
+        return $this->veiculosEstacionados;
     }
 
     public function estaLotado()
     {
-        if(count($this->carrosEstacionados) == self::VAGAS){
-            return true;
-        }else {
-            return false;
-        }
+        return count($this->veiculosEstacionados) == self::VAGAS;
     }
 
     public function estaEstacionado(Carro $carro)
     {
-        if(in_array($carro,$this->carrosEstacionados)){
-            return true;
-        }else {
-            return false;
-        }
-   }
-
-   public function estacionar(Carro $carro){
-
-    if ($this->estaLotado()){
-        throw new EstacionamentoLotadoException("Está lotado",1);
-    }else{
-        $this->carrosEstacionados[]=$carro;
+        return in_array($carro, $this->veiculosEstacionados);
     }
 
-   }
+    public function estacionar(Carro $carro)
+    {
+        if($this->estaLotado()) {
+            throw new EstacionamentoLotadoException('lotado', 1);
+        }
 
+        return $this->veiculosEstacionados[] = $carro;
+    }
 }
