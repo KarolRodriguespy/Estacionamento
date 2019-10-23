1c82d87b491cf7b408cf9064f58015bcecc19116
KarolRodriguespy <kr.araujo.12@gmail.com>
2019-10-22 21:39:29 -0300

    Classes

---
+++ Carro.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Challenges;
+
+use Challenges\Veiculos\Tipo;
+use Challenges\Veiculos\Veiculo;
+use Challenges\Veiculos\VeiculoGenerico;
+
+class Carro extends VeiculoGenerico implements Veiculo
+{
+    
+    private $placa;
+    private $modelo;
+    private $cor;
+    private $motorista;
+
+   
+    public function __construct($placa,$modelo,$cor,$motorista)
+    {
+        $this->placa=$placa;
+        $this->modelo=$modelo;
+        $this->cor=$cor;
+        $this->motorista =$motorista;
+
+    }
+  
+}
